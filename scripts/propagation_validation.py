1acbe77c6afce39fd9e2a69e90adaa1dcc0a19e9
Andreas Salzburger <asalzburger@gmail.com>
2024-09-27 16:53:24 +0200

    now with commit

--- scripts/propagation_validation.py
+++ scripts/propagation_validation.py
@@ -1,5 +1,6 @@
 import acts
 import argparse
+import geometry_gen1
 import geometry_gen2
 import particle_generation
 from acts.examples.simulation import (
@@ -11,7 +12,6 @@ from acts.examples.simulation import (
 
 u = acts.UnitConstants
 
-
 def main():
     p = argparse.ArgumentParser()
 
@@ -20,14 +20,6 @@ def main():
     p.add_argument("-j", "--threads", type=int, default=1, help="Number of Threads for parallel execution")
 
     p.add_argument("-i", "--input", type=str, default="", help="Input SQL file")
-
-    p.add_argument(
-        "-o",
-        "--output",
-        type=str,
-        default="propagation",
-        help="Output file(s) base name",
-    )
 
     p.add_argument(
         "-m", "--map", type=str, default="", help="Input file for the material map"
@@ -43,11 +35,17 @@ def main():
     # Add Particle generation related arguments
     particle_generation.add_arguments(p)
 
+    # The modes are
+    # gen1: Gen1 detector with Gen1 navigator and propagator
+    # gen2: Gen2 detector with Gen2 navigator and propagator
+    # detray_gen2: Gen2 detector with detray navigator and propagator
+    # geant4_gen1: Geant4 navigator and propagator with gen1 surface matching
+    # geant4_gen2: Geant4 navigator and propagator with gen2 surface matching
     p.add_argument(
         "--mode",
         type=str,
         default="gen2",
-        choices=["gen1", "gen2", "detray"],
+        choices=["gen1", "gen2", "detray_gen2", "geant4_gen1", "geant4_gen2"],
         help="Convert to detray detector and run detray navigation and propagation",
     )
 
@@ -69,65 +67,28 @@ def main():
         help="Write out the step objects",
     )
 
+    p.add_argument(
+        "--output-sim-hits",
+        action=argparse.BooleanOptionalAction,
+        help="Write out sim hits, only makes sense for Geant4",
+    )
+
     args = p.parse_args()
 
     gContext = acts.GeometryContext()
     logLevel = acts.logging.INFO
 
-    materialDecorator = None
-    if args.map != "":
-        print(">>> Loading a material decorator from file:", args.map)
-        materialDecorator = acts.IMaterialDecorator.fromFile(args.map)
-
-    # The propagator
-    propagatorImpl = None
-    stepper = acts.StraightLineStepper()
-
-    # Timing measurement is run if neither output in on
-    sterileRun = False
-    if not args.output_summary and not args.output_steps:
-        print(">> Timing measurement is enabled, no output is written")
-        sterileRun = True
-
-    # Build the detector for Gen1
-    if args.mode == "gen1":
-        # Build the detector for Gen1
-        from acts.examples.odd import getOpenDataDetector
-        detector, trackingGeometry, decorators = getOpenDataDetector()
-        # Set up the navigator - Gen1
-        navigator = acts.Navigator(trackingGeometry=trackingGeometry)
-        propagator = acts.Propagator(stepper, navigator)
-        propagatorImpl = acts.examples.ConcretePropagator(propagator)
-    else:
-        # Build the detector for Gen2 (also detray)
-        detector, storage = geometry_gen2.build(args, gContext, logLevel, materialDecorator)
-        if args.mode == "gen2":
-            # Set up the navigator - Gen2
-            navigatorConfig = acts.DetectorNavigator.Config()
-            navigatorConfig.detector = detector
-            navigator = acts.DetectorNavigator(navigatorConfig, logLevel)
-            propagator = acts.Propagator(stepper, navigator)
-            # And finally the propagtor implementation
-            propagatorImpl = acts.examples.ConcretePropagator(propagator)
-
-        elif args.mode == "detray":
-            # Translate the Gen2 detector to detray and compare that
-            detrayOptions = acts.detray.DetrayConverter.Options()
-            detrayOptions.convertSurfaceGrids = args.detray_surface_grids
-            detrayStore = acts.examples.traccc.convertDetectorHost(gContext, detector, detrayOptions)
-            propagatorImpl = acts.examples.traccc.createSlPropagatorHost(detrayStore, sterileRun)
-
-    # Evoke the sequence
+    # Common (to all modes): Evoke the sequence
     rnd = acts.examples.RandomNumbers(seed=args.seed)
 
-    # Build the sequencer
+    # Commom: Build the sequencer
     s = acts.examples.Sequencer(events=args.events, numThreads=args.threads)
 
-    # Add the particle gun from ACTS
+    # Common: Add the particle gun from ACTS
     addParticleGun(
         s,
         ParticleConfig(
-            num=args.tracks, pdg=acts.PdgParticle.eMuon, randomizeCharge=True
+            num=args.tracks, pdg=acts.PdgParticle.eInvalid, randomizeCharge=True
         ),
         EtaConfig(args.eta_range[0], args.eta_range[1]),
         MomentumConfig(
@@ -136,46 +97,172 @@ def main():
         rnd=rnd,
     )
 
-    # Run particle smearing
-    trackParametersGenerator = acts.examples.ParticleSmearing(
-        level=acts.logging.INFO,
-        inputParticles="particles_input",
-        outputTrackParameters="start_parameters",
-        randomNumbers=rnd,
-        sigmaD0=0.0,
-        sigmaZ0=0.0,
-        sigmaPhi=0.0,
-        sigmaTheta=0.0,
-        sigmaPtRel=0.0,
-    )
-    s.addAlgorithm(trackParametersGenerator)
+    # Timing measurement is run if neither output in on
+    sterileRun = False
+    if not args.output_summary and not args.output_steps:
+        print(">> Timing measurement is enabled, no output is written")
+        sterileRun = True
 
-    propagationAlgorithm = acts.examples.PropagationAlgorithm(
-        propagatorImpl=propagatorImpl,
-        level=acts.logging.INFO,
-        sterileLogger=sterileRun,
-        inputTrackParameters="start_parameters",
-        outputSummaryCollection="propagation_summary",
-    )
-    s.addAlgorithm(propagationAlgorithm)
+    # Material decoration for reconstruction geometry
+    materialDecorator = None
+    if args.map != "":
+        print(">>> Loading a material decorator from file:", args.map)
+        materialDecorator = acts.IMaterialDecorator.fromFile(args.map)
 
-    # Write the summary
+    # Build the acts geometry
+    actsGeometry = None
+    detectorStore = {}
+    if "gen1" in args.mode:
+        # Build the detector for Gen1
+        actsGeometry, detectorStore = geometry_gen1.build(args, gContext, logLevel, materialDecorator)
+    elif "gen2" in args.mode:
+        # Build the detector for Gen2 (also detray)
+        actsGeometry, detectorStore = geometry_gen2.build(args, gContext, logLevel, materialDecorator)
+
+
+    # Check the mode
+    print(">>> Test mode is :", args.mode)
+    # check if the mode does not contain geant4
+    if not "geant4" in args.mode:
+        # The propagator
+        propagatorImpl = None
+        stepper = acts.StraightLineStepper()
+
+        # Build the detector for Gen1
+        if args.mode == "gen1":
+            # Set up the navigator - Gen1
+            navigator = acts.Navigator(trackingGeometry=actsGeometry)
+            propagator = acts.Propagator(stepper, navigator)
+            propagatorImpl = acts.examples.ConcretePropagator(propagator)
+        else:
+            if args.mode == "gen2":
+                # Set up the navigator - Gen2
+                navigatorConfig = acts.DetectorNavigator.Config()
+                navigatorConfig.detector = actsGeometry
+                navigator = acts.DetectorNavigator(navigatorConfig, logLevel)
+                propagator = acts.Propagator(stepper, navigator)
+                # And finally the propagtor implementation
+                propagatorImpl = acts.examples.ConcretePropagator(propagator)
+
+            elif args.mode == "detray_gen2":
+                # Translate the Gen2 detector to detray and compare that
+                detrayOptions = acts.detray.DetrayConverter.Options()
+                detrayOptions.convertSurfaceGrids = args.detray_surface_grids
+                detrayStore = acts.examples.traccc.convertDetectorHost(gContext, actsGeometry, detrayOptions)
+                propagatorImpl = acts.examples.traccc.createSlPropagatorHost(detrayStore, sterileRun)
+
+        # Run particle smearing
+        trackParametersGenerator = acts.examples.ParticleSmearing(
+            level=acts.logging.INFO,
+            inputParticles="particles_input",
+            outputTrackParameters="start_parameters",
+            randomNumbers=rnd,
+            sigmaD0=0.0,
+            sigmaZ0=0.0,
+            sigmaPhi=0.0,
+            sigmaTheta=0.0,
+            sigmaPtRel=0.0,
+        )
+        s.addAlgorithm(trackParametersGenerator)
+
+        propagationAlgorithm = acts.examples.PropagationAlgorithm(
+            propagatorImpl=propagatorImpl,
+            level=acts.logging.INFO,
+            sterileLogger=sterileRun,
+            inputTrackParameters="start_parameters",
+            outputSummaryCollection="propagation_summary",
+        )
+        s.addAlgorithm(propagationAlgorithm)
+    else :
+        from acts import examples
+        from acts.examples import geant4 as acts_g4
+        print(">>> Running Geant4 simulation, buckle up...")
+        # The sensitive surface mapper
+        smmConfig = acts_g4.SensitiveSurfaceMapper.Config()
+        smmConfig.volumeMappings = []
+        smmConfig.materialMappings = ['Silicon']
+        sensitiveMapper = acts_g4.SensitiveSurfaceMapper.create(
+            smmConfig, logLevel, actsGeometry
+        )
+
+        detectorConstruction = None
+        detector = detectorStore["Detector"]
+
+        from acts import geomodel as gm
+        from acts.examples.dd4hep import DD4hepDetector
+
+        if type(detector) is DD4hepDetector:
+            from acts.examples.geant4.dd4hep import DDG4DetectorConstructionFactory
+            detectorConstruction =  DDG4DetectorConstructionFactory(detector, [])
+        elif type(detector) is gm.GeoModelTree :
+            # The GeoModel detector
+            from acts.examples.geant4.geomodel import GeoModelDetectorConstructionFactory
+            detectorConstruction = GeoModelDetectorConstructionFactory(detector, [])
+
+        #  Simulation
+        physicsList = "MaterialPhysicsList"
+        killVolume = detectorStore["Volume"]
+        killAfterTime = float("inf")
+
+        bfield = None
+
+        geant4Simulation = acts_g4.Geant4Simulation(
+            level=logLevel,
+            detectorConstructionFactory=detectorConstruction,
+            randomNumbers=rnd,
+            inputParticles="particles_input",
+            outputParticlesInitial="particales_initial",
+            outputParticlesFinal="particles_final",
+            outputSimHits="sim_hits",
+            sensitiveSurfaceMapper=sensitiveMapper,
+            magneticField=bfield,
+            physicsList=physicsList,
+            killVolume=killVolume,
+            killAfterTime=killAfterTime,
+            killSecondaries=True,
+            recordHitsOfNeutrals=True,
+            recordHitsOfSecondaries=False,
+            keepParticlesWithoutHits=False,
+        )
+        s.addAlgorithm(geant4Simulation)
+
+        # Convert the sim hits to propagation summary objects
+        simHitsToSummary = acts.examples.SimHitToSummaryConversion(
+            level=logLevel,
+            inputSimHits="sim_hits",
+            inputParticles="particales_initial",
+            outputSummaryCollection="propagation_summary",
+            surfaceByIdentifier=detectorStore["SurfaceByIdentifier"],
+        )
+        s.addAlgorithm(simHitsToSummary)
+
+        # Optionally: Write the sim hits
+        if args.output_sim_hits:
+            simHits = "sim_hits"
+            s.addWriter(
+                acts.examples.RootSimHitWriter(
+                    level=logLevel,
+                    inputSimHits=simHits,
+                    filePath=args.mode+"_sim_hits.root"),
+            )
+
+    # Common: Write the summary
     if args.output_summary:
         s.addWriter(
             acts.examples.RootPropagationSummaryWriter(
                 level=acts.logging.INFO,
                 inputSummaryCollection="propagation_summary",
-                filePath=args.output + "_propagation_summary.root",
+                filePath=args.mode + "_propagation_summary.root",
             )
         )
 
-    # Write the steps
+    # Common: Write the steps
     if args.output_steps:
         s.addWriter(
             acts.examples.RootPropagationStepsWriter(
                 level=acts.logging.INFO,
                 collection="propagation_summary",
-                filePath=args.output + "_propagation_steps.root",
+                filePath=args.mode + "_propagation_steps.root",
             )
         )
 
